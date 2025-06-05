2058c04514af3587dee59801b7be79ad302a927a
Racheloviedo <00033125@uca.edu.sv>
2025-06-04 21:19:39 -0600

    Agregando cambios

--- practica.cpp
+++ practica.cpp
@@ -11,12 +11,12 @@ using namespace std;
 int main(){
     //Tarifa luz electrica
     //Calcula la tarifa según el consumo: menor a 100 kWh, tarifa baja; entre 100 y 200 kWh, tarifa media; más de 200 kWh, tarifa alta.
-    int tarifa, consumo;
-    cout << "Por favor ingree su consumo de luz electrica (menor a 100kWh a mas de 200kwh) " << endl;
+    int consumo;
+    cout << "Por favor ingrese su consumo de luz electrica (menor a 100kWh a mas de 200kwh) " << endl;
     cin >> consumo;
     if (consumo < 100){
         cout << "Su tarifa es baja." << endl;
-    } else if (consumo >= 100 && consumo < 200){
+    } else if (consumo >= 100 && consumo <= 200){
         cout << "Su tarifa es media." << endl;
     } else if (consumo > 200){
         cout << "Su tarifa es alta." << endl;
@@ -29,14 +29,14 @@ int main(){
 //Suma de los primeros N numeros 
 //Solicita un número N al usuario y calcula la suma de los primeros N números naturales.
 int number;
+float suma = 0;
+
 cout << "Ingrese una cantidad N numeros a sumar." << endl;
 cin>> number;
-for(int i = 0; i <= number; ++i){
-    int suma;
-    suma = suma + i;
-    cout << suma << endl; 
-
+for(int i = 0; i <= number; i++){
+    suma += i; 
 }
+cout << "The sum of the first: " << number << " natural numbers is: " << suma << endl;
     return 0;
 }
 
@@ -44,9 +44,20 @@ for(int i = 0; i <= number; ++i){
 int main(){
     //Juego de adivinanza con intentos limitados 
     //El usuario tiene 5 intentos para adivinar un número aleatorio entre 1 y 50
-    int intentos = 5, num;
-    
+    int intentos, secretNumber = 43;
+    cout << "Guess the number between 1 and 50." << endl;
+    cin >> intentos;
 
+    while(intentos != secretNumber){
+        if (intentos > secretNumber){
+            cout << "You have pass from the number. Try again." << endl;
+        } else if (intentos < secretNumber){
+            cout << "The number is to low. Try again." << endl;
+        }
+        cin >> intentos
+    }
+    
+     cout << "You have guess the number, good job! " << endl;
 
     return 0;
 }
@@ -96,6 +107,9 @@ int main(){
         case 12:
         cout << "Diciembre tiene 31 dias." << endl;
         break;
+        default:
+        cout << "Opcion invalida." << endl;
+        break;
     }
 
 
@@ -105,6 +119,16 @@ int main(){
 //Ejercicio Array
 int main(){
     //Llena un arreglo con N números y cuenta cuántos son pares y cuántos son impares.
+    int numeros[7] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    //recorrer arreglo
+    for(int i = 7; i >= 0; i--){
+        if(numeros[i] % 2 == 0){
+            cout << "Numero " <<  numeros[i] << " es par. " << endl; 
+        } else if (numeros[i] % 2 != 0){
+            cout << "Numero " <<  numeros[i] << " es impar. " << endl;
+        }
+    } 
 
     return 0;
 }
